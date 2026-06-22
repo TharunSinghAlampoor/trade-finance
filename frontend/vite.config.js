5ca4f4306b7706710d6b10b4b7fa1c577d3b3b77
Alampoor Tharun Singh <tharunsingh851@gmail.com>
2026-06-22 13:04:00 +0530

    Fix Github Pages config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/trade-finance/'
-<<<<<<< HEAD
+  base: '/Trade-Finance/'
 })
-=======
-})
->>>>>>> 86a6f203216c7ebae4598ed006e22a9c63dbf8f2
